7293152523fb277a8e67442ba9d5265940550a3d
erga7080 <erik.garpenhag@gmail.com>
2026-09-21 17:24:39 +0200

    branch and skeleton for my watermarking method

--- server/src/watermarking_methods/erik_watermark.py
+++ server/src/watermarking_methods/erik_watermark.py
@@ -1,2 +1,36 @@
-from watermarking_method import WatermarkingMethod
+import pymupdf
+from watermarking_method import (
+    InvalidKeyError,
+    SecretNotFoundError,
+    WatermarkingError,
+    WatermarkingMethod,
+    load_pdf_bytes,
+)
 
+class erik_watermark(WatermarkingMethod):
+    @staticmethod
+    def get_usage() -> str:
+        return "usage"
+    
+    def add_watermark(
+        self,
+        pdf,
+        secret,
+        key,
+        position=None,
+    ) -> bytes:
+        pdf_bytes = load_pdf_bytes(pdf)
+
+        return
+        
+    def is_watermark_applicable(
+        self,
+        pdf,
+        position=None,
+    ) -> bool:
+
+        return
+
+    def read_secret(self, pdf, key) -> str:
+
+        return
