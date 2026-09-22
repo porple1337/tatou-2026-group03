3e0dc962fab64626156630764e250301d29106de
erikg <erik.garpenhag@gmail.com>
2026-09-22 15:56:49 +0200

    Created erik_watermark method and added it to the watermarking_utils dictionary for API access

--- server/src/watermarking_utils.py
+++ server/src/watermarking_utils.py
@@ -43,6 +43,7 @@ from watermarking_method import (
 )
 from add_after_eof import AddAfterEOF
 from unsafe_bash_bridge_append_eof import UnsafeBashBridgeAppendEOF
+from watermarking_methods.erik_watermark import erik_watermark
 
 # --------------------
 # Method registry
@@ -50,7 +51,8 @@ from unsafe_bash_bridge_append_eof import UnsafeBashBridgeAppendEOF
 
 METHODS: Dict[str, WatermarkingMethod] = {
     AddAfterEOF.name: AddAfterEOF(),
-    UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF()
+    UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF(),
+    erik_watermark.name: erik_watermark()
 }
 """Registry of available watermarking methods.
 
